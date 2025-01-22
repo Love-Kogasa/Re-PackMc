cf711597dc204896e90ac64393ea377b09275cf4
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2025-01-22 23:06:16 +0800

    Add files via upload

---
+++ core/block.js
@@ -0,0 +1,55 @@
+module.exports = function( id, icon, name, type = "items", components = {}, events = {}, format = "1.16.100" ){
+  var data = components
+  data[ "minecraft:display_name" ] = name
+  data[ "minecraft:material_instances" ] = {"*" : {texture: icon}}
+  var eves = events
+  var others = []
+  function item(){
+    return [{
+      path: "data/blocks/" + id.split( ":" )[1] + ".json",
+      json : {
+        "format_version": format,
+        "minecraft:block": {
+          description: {
+            identifier: id,
+            menu_category: {
+              category: type
+            }
+          },
+          components: data,
+          events: eves
+        }
+      }
+    }].concat( others )
+  }
+  function setComponents( key, value ){
+    data[ key ] = value
+  }
+  function set( key, value ){
+    data[ "minecraft:" + key ] = value
+  }
+  function setEvent( key, value ){
+    eves[ key ] = value
+  }
+  function setRecipe( table, keys, count = 1, tag = "crafting_table" ){
+    others.push( {
+      path : "data/recipes/" + id.split( ":" )[1] + "_recipe.json",
+      json : {
+        "format_version": "1.12.2",
+        "minecraft:recipe_shaped": {
+          description: {
+            identifier: id + "_recipe"
+          },
+          tags: Array.isArray( tag ) ? tag : [tag],
+          pattern: table,
+          key: keys,
+          result: {
+            item: id,
+            count: count
+          }
+        }
+      }
+    })
+  }
+  return {json:item, setComponents, set, setEvent, setRecipe }
+}
